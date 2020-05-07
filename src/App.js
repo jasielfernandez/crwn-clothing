4d7ee786f0ce7cf23785633dc94a923936d479dc
Jasiel Fernandez <jasielf12@gmail.com>
2020-05-06 23:50:39 -0500

    Added shop data and integrated collection shop preview

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
-
+import './App.css'; 
+import ShopPage from './pages/shop/shop.components.jsx';
 import HomePage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-  )
-
-
 
 
 function App() {
@@ -18,7 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

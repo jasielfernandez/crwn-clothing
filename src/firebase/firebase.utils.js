f83036fa4664a7806593756228557c08ef92df1b
Jasiel Fernandez <jasielf12@gmail.com>
2020-05-08 00:04:27 -0500

    Added Firebase Google OAuth and updated sign in page structure and styling

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  
+  apiKey: "AIzaSyCwMvaUkxLAQ4o15WoaeuopXAZQNes8Xqw",
+  authDomain: "crwn-db-7127c.firebaseapp.com",
+  databaseURL: "https://crwn-db-7127c.firebaseio.com",
+  projectId: "crwn-db-7127c",
+  storageBucket: "crwn-db-7127c.appspot.com",
+  messagingSenderId: "952888926820",
+  appId: "1:952888926820:web:f55defdfbc502559df3571",
+  measurementId: "G-9W550WYH4W"
+  
+}
+
+firebase.initializeApp(config);
+
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
